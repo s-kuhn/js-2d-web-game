4f83dca23e0d1f467fca46bb608e95f38ef141e9
Sascha Kuhn <kusa1019@hs-karlsruhe.de>
2023-01-09 23:49:49 +0100

    stage2

--- script.js
+++ script.js
@@ -1,100 +1,75 @@
-let playerState = 'idle';
-const dropdown = document.getElementById('animations');
-dropdown.addEventListener('change', function(e){
-	playerState = e.target.value;
-})
-
 const canvas = document.getElementById('canvas1');
-// short for context
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 600;
-const CANVAS_HEIGHT = canvas.height = 600;
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 700;
+let gameSpeed = 5;
+//let gameFrame = 0;
 
-// store sprite sheet image to animate with js
-const playerImage = new Image();
-playerImage.src = 'shadow_dog.png';
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = 'backgroundLayers/layer-1.png';
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = 'backgroundLayers/layer-2.png';
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = 'backgroundLayers/layer-3.png';
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = 'backgroundLayers/layer-4.png';
+const backgroundLayer5 = new Image();
+backgroundLayer5.src = 'backgroundLayers/layer-5.png';
 
-const spriteWidth = 575;
-const spriteHeight = 523;
-
-
-let gameFrame = 0;
-const staggerFrames = 5;
-spriteAnimations = [];
-const animationStates = [
-	{
-		name: 'idle',
-		frames: 7,
-	},
-	{
-		name: 'jump',
-		frames: 7,
-	},
-	{
-		name: 'fall',
-		frames: 7,
-	},
-	{
-		name: 'run',
-		frames: 9,
-	},
-	{
-		name: 'dizzy',
-		frames: 11,
-	},
-	{
-		name: 'sit',
-		frames: 5,
-	},
-	{
-		name: 'roll',
-		frames: 7,
-	},
-	{
-		name: 'bite',
-		frames: 7,
-	},
-	{
-		name: 'ko',
-		frames: 12,
-	},
-	{
-		name: 'getHit',
-		frames: 4,
-	},
-];
-
-// fill spriteAnimation array with frameposition of each animation state and corrosponding frames
-animationStates.forEach((state, index) => {
-	let frames = {
-		loc: [],
-	}
-	for (let j = 0; j < state.frames; j++){
-		let positionX = j * spriteWidth;
-		let positionY = index * spriteHeight;
-		frames.loc.push({x: positionX, y: positionY});
-	}
-	spriteAnimations[state.name] = frames;
+window.addEventListener('load', function(){
+	const slider = document.getElementById('slider');
+slider.value = gameSpeed;
+const showGameSpeed = document.getElementById('showGameSpeed');
+showGameSpeed.innerHTML =gameSpeed;
+slider.addEventListener('change', function(e){
+	gameSpeed = e.target.value;
+	showGameSpeed.innerHTML = gameSpeed;
 });
-console.log(spriteAnimations);
 
-// animation loop
-function animate(){
-	ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-
-	let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length;
-	let frameX = spriteWidth * position;
-	let frameY = spriteAnimations[playerState].loc[position].y;
-
-	//ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh) s teil was ausschneiden, d teil wo platzieren
-	ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-	if (gameFrame % staggerFrames == 0){
-		if (frameX < 6) frameX++
-		else frameX = 0;
+class Layer {
+	constructor(image, speedModifier){
+		this.x = 0;
+		this.y = 0;
+		this.width = 2400;
+		this.height = 700;
+		this.image = image;
+		this.speedModifier = speedModifier;
+		this.speed = gameSpeed * this.speedModifier;
 	}
-	
-	gameFrame++;
+
+	update(){
+		this.speed = gameSpeed * this.speedModifier;
+		//this.x = gameFrame * this.speed % this.width;
+		if (this.x <= -this.width){
+			this.x = 0;
+		}
+		this.x = this.x - this.speed;
+	}
+
+	draw(){
+		ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+		ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+
+	}
+}
+
+const layer1 = new Layer(backgroundLayer1, 0.2);
+const layer2 = new Layer(backgroundLayer2, 0.4);
+const layer3 = new Layer(backgroundLayer3, 0.6);
+const layer4 = new Layer(backgroundLayer4, 0.8);
+const layer5 = new Layer(backgroundLayer5, 1);
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5];
+
+function animate(){
+	ctx.clearRect(0, 0,CANVAS_WIDTH, CANVAS_HEIGHT)
+	gameObjects.forEach(object => {
+		object.update();
+		object.draw();
+	});
+	//gameFrame--;
 	requestAnimationFrame(animate);
 };
+animate();
+});
 
-animate()
