085ad9ddf3a43e7a14a2863365dfd44ed7060cdf
s-kuhn <s.k.96@outlook.de>
2023-01-10 20:55:43 +0100

    add enemy1 movement

--- enemy3.js
+++ enemy3.js
@@ -15,13 +15,13 @@ class Enemy {
 		this.speed = Math.random() * 4 + 1;
 		this.spriteWidth = 218;
 		this.spriteHeight = 177;
-		this.width = this.spriteWidth / 2.5;
-		this.height = this.spriteHeight / 2.5;
-		this.x = Math.random() * (canvas.width - this.width);
-		this.y = Math.random() * (canvas.height - this.height);
+		//this.width = this.spriteWidth / 2.5;
+		//this.height = this.spriteHeight / 2.5;
+		//this.x = Math.random() * (canvas.width - this.width);
+		//this.y = Math.random() * (canvas.height - this.height);
 		this.frame = 0;
-		this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-		this.angle = 0;Math.random() * 500;
+		//this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+		//this.angle = 0;Math.random() * 500;
 		this.angleSpeed = Math.random() * 1.5 + 0.5;
 		this.curve = Math.random() * 200 + 50;
 	}
@@ -38,7 +38,7 @@ class Enemy {
 	draw(){
 		ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
 	}
-};
+}
 
 for (let i = 0; i < numberOfEnemies; i++){
 	enemiesArray.push(new Enemy());
