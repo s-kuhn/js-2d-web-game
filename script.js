032d69d3922feffb6c58a8bd52aead13f9207b2f
Sascha Kuhn <kusa1019@hs-karlsruhe.de>
2023-01-16 03:00:33 +0100

    fin game, bug with enemy not drawn

--- script.js
+++ script.js
@@ -2,176 +2,207 @@
 window.addEventListener('load', (event) => {
   const canvas = document.getElementById('canvas1');
   const ctx = canvas.getContext('2d');
-  canvas.width = 500;
-  canvas.height = 800;
+  canvas.width = 800;
+  canvas.height = 720;
+  let enemies = [];
 
-  class Game {
-    // not use global variables so pass them
-    constructor(ctx, width, height) {
-      this.ctx = ctx;
-      this.width = width;
-      this.height = height;
-      this.enemies = [];
-      this.enemyInterval = 500;
-      this.enemyTimer = 0;
-      this.enemyTypes = ['worm', 'ghost', 'spider'];
-    }
-    update(deltaTime) {
-      // TODO maybe run less often, maybe in if stmt
-      this.enemies = this.enemies.filter((object) => !object.markedForDeletion);
-      if (this.enemyTimer > this.enemyInterval) {
-        this.#addNewEnemy();
-        this.enemyTimer = 0;
-        //console.log(this.enemies);
-      } else {
-        this.enemyTimer += deltaTime;
-      }
-      this.enemies.forEach((object) => object.update(deltaTime));
-    }
-    draw() {
-      this.enemies.forEach((object) => object.draw(this.ctx));
-    }
+  class InputHandler {
+    constructor() {
+      this.keys = [];
+      // lexical scoping
+      window.addEventListener('keydown', (e) => {
+        if (
+          (e.key === 'ArrowDown' ||
+            e.key === 'ArrowUp' ||
+            e.key === 'ArrowLeft' ||
+            e.key === 'ArrowRight') &&
+          this.keys.indexOf(e.key) === -1
+        ) {
+          this.keys.push(e.key);
+        }
+      });
 
-    // # private only from game class
-    #addNewEnemy() {
-      const randomEnemy =
-        this.enemyTypes[Math.floor(Math.random() * this.enemyTypes.length)];
-      // pass current game object with this
-      if (randomEnemy == 'worm') this.enemies.push(new Worm(this));
-      else if (randomEnemy == 'ghost') this.enemies.push(new Ghost(this));
-      else if (randomEnemy == 'spider') this.enemies.push(new Spider(this));
-
-      /* layer by height
-      this.enemies.sort(function (a, b) {
-        return a.y - b.y;
-      });*/
+      window.addEventListener('keyup', (e) => {
+        if (
+          e.key === 'ArrowDown' ||
+          e.key === 'ArrowUp' ||
+          e.key === 'ArrowLeft' ||
+          e.key === 'ArrowRight'
+        ) {
+          this.keys.splice(this.keys.indexOf(e.key), 1);
+        }
+      });
     }
   }
 
-  class Enemy {
-    constructor(game) {
-      this.game = game;
-      this.markedForDeletion = false;
-      this.frameX;
-      this.maxFrame = 5;
-      this.frameInterval = 100;
-      this.frameTimer = 0;
+  class Player {
+    constructor(gameWidth, gameHeight) {
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.width = 200;
+      this.height = 200;
+      this.x = 0;
+      this.y = this.gameHeight - this.height;
+      this.image = playerImage;
+      this.frameX = 0;
+      this.frameY = 0;
+      this.maxFrameX = 0;
+      this.veloX = 0;
+      this.veloY = 0;
+      this.weight = 1;
     }
-    update(deltaTime) {
-      this.x -= this.veloX * deltaTime;
-      // remove if enemy is out of frame
-      if (this.x < 0 - this.width) this.markedForDeletion = true;
-      if (this.frameTimer > this.frameInterval) {
-        if (this.frameX < this.maxFrame) this.frameX++;
-        else this.frameX = 0;
-        this.frameTimer = 0;
-      } else {
-        this.frameTimer += deltaTime;
-      }
-    }
-    draw(ctx) {
-      // will be this.ctx
-      ctx.drawImage(
+    draw(context) {
+      context.fillStyle = 'white';
+      context.fillRect(this.x, this.y, this.width, this.height);
+      context.drawImage(
         this.image,
-        this.frameX * this.spriteWidth,
-        0,
-        this.spriteWidth,
-        this.spriteHeight,
+        this.frameX * this.width,
+        this.frameY * this.height,
+        this.width,
+        this.height,
         this.x,
         this.y,
         this.width,
         this.height,
       );
     }
-  }
+    update(input) {
+      // TODO jump while run
+      if (input.keys.indexOf('ArrowRight') > -1) {
+        this.veloX = 5;
+      } else if (input.keys.indexOf('ArrowLeft') > -1) {
+        this.veloX = -5;
+      } else if (input.keys.indexOf('ArrowUp') > -1 && this.onGround()) {
+        this.veloY = -32;
+      } else {
+        this.veloX = 0;
+      }
+      // TODO add more animation frames
+      // horizontal movement
+      this.x += this.veloX;
+      if (this.x < 0) this.x = 0;
+      else if (this.x > this.gameWidth - this.width)
+        this.x = this.gameWidth - this.width;
 
-  class Worm extends Enemy {
-    constructor(game) {
-      super(game); //run parrent constructor
-      this.spriteWidth = 229;
-      this.spriteHeight = 171;
-      this.width = this.spriteWidth * 0.5;
-      this.height = this.spriteHeight * 0.5;
-      this.x = this.game.width; // ganz rechts einsetzen
-      this.y = this.game.height - this.height; // y von oben nach unten aufsteigend, deshalb ganz unten
-      // no need to use getElementById()
-      this.image = worm;
-      this.veloX = Math.random() * 0.1 + 0.1;
+      // vertical movement
+      this.y += this.veloY;
+      if (!this.onGround()) {
+        this.veloY += this.weight;
+        this.frameY = 1;
+      } else {
+        this.veloY = 0;
+        this.frameY = 0;
+      }
+      if (this.y > this.gameHeight - this.height)
+        this.y = this.gameHeight - this.height;
     }
-  }
-
-  class Ghost extends Enemy {
-    constructor(game) {
-      super(game); //run parrent constructor
-      this.spriteWidth = 261;
-      this.spriteHeight = 209;
-      this.width = this.spriteWidth * 0.5;
-      this.height = this.spriteHeight * 0.5;
-      this.x = this.game.width;
-      this.y = Math.random() * this.game.height * 0.6;
-      this.image = ghost;
-      this.veloX = Math.random() * 0.2 + 0.1;
-      this.angle = 0;
-      this.curve = Math.random() * 3;
-    }
-    update(deltaTime) {
-      super.update(deltaTime);
-      this.y += Math.sin(this.angle) * this.curve;
-      this.angle += 0.04;
-    }
-    draw(ctx) {
-      ctx.globalAlpha = 0.5;
-      super.draw(ctx); // extend parrent method
-      ctx.globalAlpha = 1;
+    onGround() {
+      return this.y >= this.gameHeight - this.height;
     }
   }
 
-  class Spider extends Enemy {
-    constructor(game) {
-      super(game); //run parrent constructor
-      this.spriteWidth = 310;
-      this.spriteHeight = 175;
-      this.width = this.spriteWidth * 0.5;
-      this.height = this.spriteHeight * 0.5;
-      this.x = Math.random() * this.game.width;
-      this.y = 0 - this.height;
-      this.image = spider;
-      this.veloX = 0;
-      this.veloY = Math.random() * 0.1 + 0.1;
-      this.maxDown = Math.random() * this.game.height;
+  class Background {
+    constructor(gameWidth, gameHeight) {
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.image = backgroundImage;
+      this.x = 0;
+      this.y = 0;
+      this.width = 2400;
+      this.height = 720;
+      this.veloX = 4;
     }
-    update(deltaTime) {
-      super.update(deltaTime);
-      if (this.y < 0 - this.height * 2) this.markedForDeletion = true;
-      this.y += this.veloY * deltaTime;
-      if (this.y > this.maxDown) {
-        this.veloY *= -1;
+    draw(context) {
+      context.drawImage(this.image, this.x, this.y, this.width, this.height);
+      context.drawImage(
+        this.image,
+        this.x + this.width - this.veloX,
+        this.y,
+        this.width,
+        this.height,
+      );
+    }
+    update(input) {
+      // TODO stop if not moving
+      this.x -= this.veloX;
+      if (this.x < 0 - this.width) {
+        this.x = 0;
       }
     }
-    draw(ctx) {
-      ctx.beginPath();
-      ctx.moveTo(this.x + this.width * 0.5, 0);
-      ctx.lineTo(this.x + this.width * 0.5, this.y + 10);
-      ctx.stroke();
-      super.draw(ctx);
+  }
+
+  class Enemy {
+    constructor(gameWidth, gameHeight) {
+      this.gameWidth = gameWidth;
+      this.gameHeight = gameHeight;
+      this.weight = 160;
+      this.height = 119;
+      this.image = enemyImage;
+
+      //this.markedForDeletion = false;
+      this.frameX = 0;
+      /*this.maxFrame = 5;
+      this.frameInterval = 100;
+      this.frameTimer = 0;*/
+      this.x = this.gameWidth;
+      this.y = this.gameHeight - this.height;
+    }
+
+    draw(context) {
+      context.fillStyle = 'white';
+      context.fillRect(this.x, this.y, this.width, this.height);
+      context.drawImage(
+        this.image,
+        this.frameX * this.width,
+        0,
+        this.width,
+        this.height,
+        this.x,
+        this.y,
+        this.width,
+        this.height,
+      );
+    }
+
+    update() {
+      this.x--;
     }
   }
 
-  const game = new Game(ctx, canvas.width, canvas.height);
-  let lastTime = 1;
+  function handleEnemies(deltaTime) {
+    if (enemyTimer > enemyInterval) {
+      enemies.push(new Enemy(canvas.width, canvas.height));
+      enemyTimer = 0;
+    } else {
+      enemyTimer += deltaTime;
+    }
+    enemies.forEach((enemy) => {
+      enemy.draw(ctx);
+      console.log(enemy.x);
+      enemy.update();
+    });
+  }
+
+  function displayStatusText() {}
+
+  const input = new InputHandler();
+  const player = new Player(canvas.width, canvas.height);
+  const background = new Background(canvas.width, canvas.height);
+
+  // TODO fix framerate
+  let lastTime = 0;
+  let enemyTimer = 0;
+  let enemyInterval = 1000;
   function animate(timeStamp) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    // generate new Frame every deltaTime ms
     const deltaTime = timeStamp - lastTime;
     lastTime = timeStamp;
-    // debug gameresponsiveness usually 16ms
-    //console.log(deltaTime);
-    game.update(deltaTime);
-    game.draw();
-
+    //background.draw(ctx);
+    //background.update(input);
+    //player.draw(ctx);
+    player.update(input);
+    handleEnemies(deltaTime);
     requestAnimationFrame(animate);
   }
   animate(0);
 });
-
-// TODO object pooling enemies
